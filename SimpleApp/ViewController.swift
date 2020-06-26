56136f9c3de439c4a39adf06575ee59697e166b1
eduardo mancilla <eduardo.mancilla@upax.com.mx>
2020-06-26 09:19:43 -0500

    Initial Commit

---
+++ SimpleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by eduardo mancilla on 26/06/20.
+//  Copyright © 2020 eduardo mancilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
